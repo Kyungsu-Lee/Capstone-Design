247cd21cd09f30d70b33953862c7a0943e152239
lmasi <lmasi@naver.com>
2017-01-17 11:57:36 +0530

    init

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	WebCamTexture cams;
+	GameObject oj;
+
+	// Use this for initialization
+	void Start () {
+	
+		cams = new WebCamTexture ();
+		oj.GetComponent<Renderer> ().material.mainTexture = cams;
+		cams.Play ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
